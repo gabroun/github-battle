088d2ed9cd56717f0f17fd59926d8943e7ad48a4
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2018-09-12 07:38:16 +0100

    implementing ajax request

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function SelectLanguage(props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -29,10 +30,19 @@ class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  //lifecyle event
+  //compoentnDidMount going to be invoked by react when the component shown to the view (mount to the screen)
+  //inside this component is gonig to be where you make your ajax requests
+  componentDidMount() {
+    api.fetchPopularRepos(this.state.selectedLanguage).then(function(repos) {
+      console.log(repos);
+    });
   }
   updateLanguage(lang) {
     this.setState(function() {
