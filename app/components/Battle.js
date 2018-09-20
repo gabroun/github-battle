e8e8b426e4e75d3186c9d3a4983c2528054c958a
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2018-09-20 07:39:22 +0100

    form creation

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,8 +1,103 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    var value = event.target.value;
+
+    this.setState(function() {
+      return {
+        username: value
+      };
+    });
+  }
+  render() {
+    return (
+      <form className="column">
+        <label className="header" htmlFor="username">
+          {this.props.label}
+        </label>
+        {/* we want to bind the value of our state to the input field  */}
+        <input
+          id="username"
+          placeholder="Github usernmae"
+          type="text"
+          autoComplete="off"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
 
 class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, username) {
+    //as we using this keyword in the function we need to bind in the constructor
+    //we want this keyword in the function to always refere to our instance in the constructor which is the component
+    //setState will return an object
+    this.setState(function() {
+      var newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] =
+        'https://github.com/' + username + '.png?size=200';
+
+      return newState;
+    });
+  }
+
   render() {
-    return <div>Battle</div>;
+    var playerOneName = this.state.playerOneName;
+    var playerTwoName = this.state.playerTwoName;
+    return (
+      <div>
+        <div className="row">
+          {!playerOneName && (
+            <PlayerInput
+              id="playerOne"
+              label="Player One"
+              onSubmit={this.handleSubmit}
+            />
+          )}
+          {!playerTwoName && (
+            <PlayerInput
+              id="playerTwo"
+              label="Player Two"
+              onSubmit={this.handleSubmit}
+            />
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
