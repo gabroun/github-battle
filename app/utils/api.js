ddf01273d191acb2952a680265acc02d39f4e50d
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2018-10-16 07:53:25 +0100

    api file refactor to es6

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,44 +1,37 @@
-var axios = require('axios');
+const axios = require('axios');
 
-var id = 'your_client_id';
-var secret = 'your_secret_id';
-var params = '?client_id=' + id + '&client_secret=' + secret;
+const id = 'your_client_id';
+const secret = 'your_secret_id';
+const params = `?client_id=${id}&client_secret=${secret}`;
 
 function getProfile(username) {
   //so the following will make a get request to github api and axios return us a promise
   //and with promises the following will return us an object
   return (
     axios
-      .get('https://api.github.com/users/' + username + params)
+      .get(`https://api.github.com/users/${username}${params}`)
       //when making requestion to external api its not going to return us the response immediately so by using axios we telling the app to asynchronousely to call the api and once its been returned then to invoke the function and pass us whatever the information
-      .then(function(user) {
-        return user.data;
-      })
+      .then(({ data }) => data)
   );
 }
 
 function getRepos(username) {
   return axios.get(
-    'https://api.github.com/users/' +
-      username +
-      '/repos' +
-      params +
-      '&per_page=100'
+    `https://api.github.com/users/${username}/repos${params}&per_page=100`
   );
 }
 
 function getStars(repos) {
-  //reduce allow us to take an array and reduce it to a single value
-  return repos.data.reduce(function(count, repos) {
-    return count + repos.stargazers_count;
-  }, 0);
+  return repos.data.reduce(
+    (count, { stargazers_count }) => count + stargazers_count,
+    0
+  );
 }
 
-function calculateScore(profile, repos) {
-  var followers = profile.followers;
+function calculateScore({ followers }, repos) {
   var totalStars = getStars(repos);
 
-  return followers * 3 + totalStars;
+  return totalStars * 3 + followers;
 }
 
 function handleError(error) {
@@ -48,35 +41,28 @@ function handleError(error) {
 
 function getUserData(player) {
   //takes in an array of promises, once they are all resolved then it going to call the function
-  return axios.all([getProfile(player), getRepos(player)]).then(function(data) {
-    var profile = data[0];
-    var repos = data[1];
-
-    return {
-      profile: profile,
-      score: calculateScore(profile, repos)
-    };
-  });
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
+      profile,
+      score: calculateScore(profile, repos),
+    })
+  );
 }
 
 function sortPlayer(players) {
-  return players.sort(function(a, b) {
-    return b.score - a.score;
-  });
+  return players.sort((a, b) => b.score - a.score);
 }
+
 //anytime we going to interact with external api inside this project, we going to have a bunce of methods in this object in order to do that
 module.exports = {
-  battle: function(players) {
-    return axios
-      .all(players.map(getUserData))
+  battle(players) {
+    return Promise.all(players.map(getUserData))
       .then(sortPlayer)
       .catch(handleError);
   },
-  fetchPopularRepos: function(language) {
+  fetchPopularRepos(language) {
     var encodedURI = window.encodeURI(
-      'https://api.github.com/search/repositories?q=stars:>1+language:' +
-        language +
-        '&sort=stars&order=desc&type=Repositories'
+      `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
     );
 
     //this going to return us a promise
@@ -84,9 +70,7 @@ module.exports = {
       axios
         .get(encodedURI)
         //this going to be invoked when the request to the specific url has been resolved and finished to be passed the response and then we going to return response.data.items
-        .then(function(response) {
-          return response.data.items;
-        })
+        .then(({ data }) => data.items)
     );
-  }
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,32 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './app/index.js',
+  entry: ['@babel/polyfill', './app/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     //Cannot get /popular component when refreshing
     //what's happening is the browser making a request to your server in order to get the assets for /popular the are none because we using react router to handle all of the rounting
     //the publicPath property let us set the base path for all of the assets
-    publicPath: '/'
+    publicPath: '/',
   },
   module: {
     //rules for the loaders of the transformation we want to make
     rules: [
       //babel is a code transformer
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-    ]
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
   },
   //what the devServer property does is instead of requesting the assets of /popular (after the page is refreshed) it wll redirect us to the basepath as setup up and then react router will load the route for /popular
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       //pass a template (the html file) we want to use
-      template: 'app/index.html'
-    })
+      template: 'app/index.html',
+    }),
   ],
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
